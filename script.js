873b83d5a87a2228c440f2853f0d440c15bd0fc8
Jhonen Valant <jhonendvalant@gmail.com>
2023-12-06 11:35:49 -0600

    Add basic interface and number input

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const calculatorElement = document.getElementById("calculator");
+const [calculatorHistory, calculatorDisplayElement] = document.querySelector(".calculator-display").children;
+
+calculatorElement.addEventListener("click", event => handleInput(event.target));
+
+const calculatorDisplay = {
+    element: calculatorDisplayElement,
+    currentNum: 0,
+    empty: true,
+    update: function() {
+        this.element.textContent = this.currentNum;
+    },
+    clear: function() {
+        this.currentNum = 0;
+        this.empty = true;
+        this.update();
+    },
+    pushNum: function(num) {
+        if (this.empty) {
+            this.currentNum = num;
+            this.empty = false;
+        } else {
+            this.currentNum = this.currentNum * 10 + num;
+        }
+
+        this.update();
+    }
+}
+
+function handleInput(element) {
+    if (element.nodeName !== "BUTTON")
+        return;
+
+    if (Number(element.innerText)) {
+        calculatorDisplay.pushNum(Number(element.innerText));
+    }
+
+    switch(element.innerText.toLowerCase()) {
+        case "ac":
+            calculatorDisplay.clear();
+    }
+}
+
 function add(first, second) {
     return first + second;
 }
@@ -12,4 +55,17 @@ function multiply(first, second) {
 
 function divide(first, second) {
    return first / second; 
+}
+
+function operate(first, second, operator) {
+    switch(operator) {
+        case "+":
+            return add(first, second);
+        case "-":
+            return subtract(first, second);
+        case "*":
+            return multiply(first, second);
+        case "/":
+            return divide(first, second);
+    }
 }
